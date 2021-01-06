a1c6146f1f3c8748fa67571ecbfad594692a5ee0
Henning Odén <henning.oden@outlook.com>
2021-01-06 18:52:24 +0100

    Removed @ComponentScan annotation

--- StockWatch.Backend/src/main/java/com/henning/oden/java/StockAlert/Backend/Application.java
+++ StockWatch.Backend/src/main/java/com/henning/oden/java/StockAlert/Backend/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.henning.oden.java.StockAlert.Backend")
 public class Application {
 
 	public static void main(String[] args) {
